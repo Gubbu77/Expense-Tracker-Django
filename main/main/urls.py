2b9627f3d8e21a5930f23ea7b1e76c974d1e6000
Indrajith.vs <91895505+Gubbu77@users.noreply.github.com>
2024-01-07 20:16:45 +0530

    Update urls.py

--- main/main/urls.py
+++ main/main/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('expense_tracker.urls')),
+    path('', include('expense_tracker.urls'))
 ]
